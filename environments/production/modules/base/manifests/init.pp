c62de7db733d667f3d9590006545d8612b475fd8
root <root@ip-172-31-44-230.us-west-2.compute.internal>
2015-06-05 08:17:03 -0400

    Cleaning up unnecessary test modules

--- environments/production/modules/base/manifests/init.pp
+++ environments/production/modules/base/manifests/init.pp
@@ -1,8 +1,7 @@
 class base {
 
-        include base::first
         include base::default-directories
         include base::mount-partition
-	#include base::cron-puppet
+	include base::cron-puppet
 
 }
