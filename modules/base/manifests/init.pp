56a14724bf206b87bd3c40c99cbde13f3e46d919
root <root@ip-172-31-44-230.us-west-2.compute.internal>
2015-06-05 04:48:42 -0400

    starting django

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -3,7 +3,6 @@ class base {
         include base::first
         include base::default-directories
         include base::mount-partition
-#	include base::mountpartition
-	include base::cron-puppet
+	#include base::cron-puppet
 
 }
---
+++ modules/django/manifests/init.pp
@@ -0,0 +1,9 @@
+class django {
+
+        include django::packages
+#        include django::default-directories
+        include django::epel
+        include django::django
+
+}
+
