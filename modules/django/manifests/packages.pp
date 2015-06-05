8375e27b4395a8a30e64cfcddecc464756c3cab5
root <root@ip-172-31-44-230.us-west-2.compute.internal>
2015-06-05 05:48:05 -0400

    Django RPMs for Centos installed

--- modules/django/manifests/packages.pp
+++ modules/django/manifests/packages.pp
@@ -6,5 +6,36 @@ class django::packages {
         package { $required_packages:
                 ensure  => latest,
         }
-}
 
+	Package {
+		allow_virtual => true,
+	}
+
+
+	if (($operatingsystem == 'RedHat') or ($operatingsystem == 'CentOS')){
+		if $operatingsystemmajrelease == '7' {
+                        package { 'python-django-bash-completion':
+                                ensure => installed,
+                                provider => 'rpm',
+                                source => '/tmp/python-django-bash-completion-1.8-1.el7.centos.noarch.rpm',
+                                require  => File["/tmp/python-django-bash-completion-1.8-1.el7.centos.noarch.rpm"],
+                        }
+                        file { "/tmp/python-django-bash-completion-1.8-1.el7.centos.noarch.rpm":
+                                source => 'puppet:///modules/django/python-django-bash-completion-1.8-1.el7.centos.noarch.rpm',
+                                links => follow,
+                        }
+                        package { 'python-django':
+                                ensure => installed,
+                                provider => 'rpm',
+                                source => '/tmp/python-django-1.8-1.el7.centos.noarch.rpm',
+                                require  => [ File["/tmp/python-django-1.8-1.el7.centos.noarch.rpm"], Package['python-django-bash-completion'] ]
+#				require => Package['python-django-bash-completion'],
+                        }
+                        file { "/tmp/python-django-1.8-1.el7.centos.noarch.rpm":
+                                source => 'puppet:///modules/django/python-django-1.8-1.el7.centos.noarch.rpm',
+                                links => follow,
+                        }
+                }
+
+	}
+}
